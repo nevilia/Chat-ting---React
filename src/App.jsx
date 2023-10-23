618f33fc91275ca815263327f6ac64cf68560ee5
nevilia <winter04nevilia@gmail.com>
2023-10-24 00:08:26 +0530

    Added Protected Routes, made useAuth custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import PrivateRoutes from "./components/PrivateRoutes"
+import {AuthProvider} from "./utils/AuthContext"
+
 import Room from "./pages/Room"
+import LoginPage from "./pages/LoginPage"
+
 
 function App() {
   return (
     <>
-      <Room/>
+    <Router>
+      <AuthProvider>
+
+        <Routes>
+          <Route path="/login" element={<LoginPage/>} />
+
+          <Route element={<PrivateRoutes/>} > 
+            <Route path="/" element={<Room/>} />
+          </Route>
+          
+        </Routes>
+
+      </AuthProvider>
+    </Router>
     </>
   )
 }
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,23 @@
+import React, {useEffect} from 'react'
+import { useAuth } from '../utils/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
+const LoginPage = () => {
+  const {user} = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(user) {
+      navigate('/')
+    }
+  }, [])
+
+
+  return (
+    <div>
+        LogIn
+    </div>
+  )
+}
+
+export default LoginPage
